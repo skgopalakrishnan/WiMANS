66733b7460401d5a0c6eeebf426a096ef1308415
Shreevanth Gopalakrishnan <shreevanth@gmail.com>
2025-03-25 13:16:41 +0000

    Added Linux-specific paths to the dataset and other config parameters

--- benchmark/wifi_csi/preset.py
+++ benchmark/wifi_csi/preset.py
@@ -19,37 +19,41 @@ preset = {
     "device" : device,
     #
     ## define model
-    "model": "THAT",                                    # "ST-RF", "MLP", "LSTM", "CNN-1D", "CNN-2D", "CLSTM", "ABLSTM", "THAT"
+    "model": "ABLSTM",                                  # "ST-RF", "MLP", "LSTM", "CNN-1D", "CNN-2D", "CLSTM", "ABLSTM", "THAT"
+    #
+    ## define train-test-split
+    "train_test_split": 0.3,                            # ratio of test set
     #
     ## define task
     "task": "activity",                                 # "identity", "activity", "location"
     #
     ## number of repeated experiments
-    "repeat": 4,
+    "repeat": 1,
     #
     ## path of data and model weights
     "path": {
-        "data_x": "/Users/shreevanth/Documents/PhD_work/Adversarial_CSI/data/WiMANS/dataset/wifi_csi/amp",                     # directory of CSI amplitude files
-        "data_y": "/Users/shreevanth/Documents/PhD_work/Adversarial_CSI/data/WiMANS/dataset/annotation.csv",                   # path of annotation file
-        "save": "/Users/shreevanth/Documents/PhD_work/Adversarial_CSI/data/WiMANS/dataset/result.json",                        # path to save results
-        "model_wt": "/Users/shreevanth/Documents/PhD_work/Adversarial_CSI/third_party/WiMANS/benchmark/wifi_csi/saved_models", # path to save results
+        "data_x": "./data/WiMANS/dataset/wifi_csi/amp",                     # directory of CSI amplitude files
+        "data_y": "./data/WiMANS/dataset/annotation.csv",                   # path of annotation file
+        "save": "./data/WiMANS/dataset/result.json",                        # path to save results
+        "model_wt": "./third_party/WiMANS/benchmark/wifi_csi/saved_models", # path to save results
     },
     #
     ## data selection for experiments
     "data": {
         # "num_users": ["0", "1", "2", "3", "4", "5"],    # select number(s) of users, (e.g., ["0", "1"], ["2", "3", "4", "5"])
-        "num_users": ["1"],                             # select number(s) of users, (e.g., ["0", "1"], ["2", "3", "4", "5"])
-        "wifi_band": ["2.4"],                           # select WiFi band(s) (e.g., ["2.4"], ["5"], ["2.4", "5"])
-        "environment": ["classroom"],                   # select environment(s) (e.g., ["classroom"], ["meeting_room"], ["empty_room"])
-        "length": 3000,                                 # default length of CSI (3 seconds capture at 1000 Hz)
+        "num_users": ["2"],                               # select number(s) of users, (e.g., ["0", "1"], ["2", "3", "4", "5"])
+        "wifi_band": ["2.4", "5"],                        # select WiFi band(s) (e.g., ["2.4"], ["5"], ["2.4", "5"])
+        "environment": ["classroom"],                     # select environment(s) (e.g., ["classroom"], ["meeting_room"], ["empty_room"])
+        "length": 3000,                                   # default length of CSI (3 seconds capture at 1000 Hz)
     },
     #
     ## hyperparameters of models
     "nn": {
         "lr": 1e-3,                                     # learning rate
-        "epoch": 2,                                   # number of epochs
-        "batch_size": 128,                              # batch size
+        "epoch": 200,                                   # number of epochs
+        "batch_size": 64,                               # batch size
         "threshold": 0.5,                               # threshold to binarize sigmoid outputs
+        "early_stop": 1000,                               # early stopping patience
     },
     #
     ## encoding of activities and locations
